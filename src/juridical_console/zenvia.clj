e171de5037f3d82414120cbfb7a7a30d854ca728
Yago Luiz <yago.luiz@hotmail.com>
2025-06-15 13:58:57 -0300

    Add error handling to SMS sending function

--- src/juridical_console/zenvia.clj
+++ src/juridical_console/zenvia.clj
@@ -4,17 +4,21 @@
             [org.httpkit.client :as http]))
 
 (defn send-sms-message [process-count]
-  (let [message (str "Atenção! Você tem um total de " process-count " processo(s) não analisado(s). Acesse https://bit.ly/3gtEHEB para mais informações.")
-        payload {:from     (config/senvia-send-sms-from)
-                 :to       (config/senvia-send-sms-to)
-                 :contents [{:type "text" :text message}]}
-        response (http/request {:url     (config/senvia-send-sms-url)
-                                :method  :post
-                                :headers {"x-api-token" (config/senvia-api-token)}
-                                :body    (json/generate-string payload)})]
-    (println (str "##### Zenvia response #####" \n
-                  "Status: " (:status @response) \n
-                  "Body: " (json/parse-string (:body @response) true)))
-    (if (= 200 (:status @response))
-      {:sms-sent? true}
+  (try
+    (let [message (str "Atenção! Você tem um total de " process-count " processo(s) não analisado(s). Acesse https://bit.ly/3gtEHEB para mais informações.")
+          payload {:from     (config/senvia-send-sms-from)
+                   :to       (config/senvia-send-sms-to)
+                   :contents [{:type "text" :text message}]}
+          response (http/request {:url     (config/senvia-send-sms-url)
+                                  :method  :post
+                                  :headers {"x-api-token" (config/senvia-api-token)}
+                                  :body    (json/generate-string payload)})]
+      (println (str "##### Zenvia response #####" \n
+                    "Status: " (:status @response) \n
+                    "Body: " (json/parse-string (:body @response) true)))
+      (if (= 200 (:status @response))
+        {:sms-sent? true}
+        {:sms-sent? false}))
+    (catch Exception e
+      (println (str "##### Zenvia error #####\n" (.getMessage e)))
       {:sms-sent? false})))
