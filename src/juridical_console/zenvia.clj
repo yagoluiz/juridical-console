f04e86de8cf6dc16e134cd1b8411a0d157cfc381
Yago Luiz <yago.luiz@hotmail.com>
2025-06-15 15:23:30 -0300

    Refactor SMS message formatting

--- src/juridical_console/zenvia.clj
+++ src/juridical_console/zenvia.clj
@@ -5,13 +5,14 @@
 
 (defn send-sms [process-count]
   (try
-    (let [message (str "Atenção! Você tem um total de " process-count " processo(s) não analisado(s). Acesse https://bit.ly/3gtEHEB para mais informações.")
+    (let [message (str "Atenção! Você tem um total de " process-count " processo(s) não analisado(s)."
+                       " Acesse https://bit.ly/3gtEHEB para mais informações.")
           payload {:from     (config/senvia-sms-from)
                    :to       (config/senvia-sms-to)
                    :contents [{:type "text" :text message}]}
           response (http/request {:url     (config/senvia-send-sms-url)
                                   :method  :post
-                                  :headers {"x-api-token" (config/senvia-api-token)
+                                  :headers {"x-api-token"  (config/senvia-api-token)
                                             "Content-Type" "application/json"}
                                   :body    (json/generate-string payload)})]
       (println (str "##### Zenvia response ##### \n"
